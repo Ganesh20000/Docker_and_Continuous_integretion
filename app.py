76fe206972810d701d2d92f48db597120b2b9c61
Ganesh20000 <ganeshkarli19@gmail.com>
2025-12-03 22:47:03 +0530

    done

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -23,5 +22,3 @@ def greet():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
