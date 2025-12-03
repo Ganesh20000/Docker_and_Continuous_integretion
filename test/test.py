d8c7c6b9457926db1c890480d93db40ce30c7170
Ganesh20000 <ganeshkarli19@gmail.com>
2025-12-03 11:57:42 +0530

    one last try

--- test/test.py
+++ test/test.py
@@ -1,19 +1,16 @@
-from src.math_operation import add,sub
 
-
+from src.math_operation import add, sub
 
 def test_add():
-    assert add(2,3)==5
-    assert add(1,3)==4
-    assert add(3,4)==7
-    assert add(1,2)==3
-
-
+    assert add(2, 3) == 5
+    assert add(1, 3) == 4
+    assert add(3, 4) == 7
+    assert add(1, 2) == 3
 
 def test_sub():
-    assert sub(5,2)==3
-    assert sub(2,3)==-1
-    assert sub(1,1)==0
-    assert sub(3,2)==1
-    assert sub(5,3)==2
-    assert sub(10,4)==6
+    assert sub(5, 2) == 3
+    assert sub(2, 3) == -1
+    assert sub(1, 1) == 0
+    assert sub(3, 2) == 1
+    assert sub(5, 3) == 2
+    assert sub(10, 4) == 6
